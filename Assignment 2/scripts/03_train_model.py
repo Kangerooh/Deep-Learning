6e1cf964d56881e72d8e024277bfdec3b5b29ed4
Kangerooh <kaspklink@gmail.com>
2026-05-26 17:38:14 +0200

    Model suggestion

--- Assignment 2/scripts/03_train_model.py
+++ Assignment 2/scripts/03_train_model.py
@@ -1,4 +1,9 @@
 import numpy as np
+"""
+First idea for the model is to use EEGNet. This is a model that is specifically designed for EEG data.
+It should be a good fit for the data that we want to predict and process.
+https://arxiv.org/abs/1611.08024
+"""
 
 data = np.load("data/processed/intra_train.npz")
 
