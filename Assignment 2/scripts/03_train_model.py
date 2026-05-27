16882216a2254012c83595a4113021495885482e
Kangerooh <kaspklink@gmail.com>
2026-05-27 09:50:52 +0200

    Bug fix regarding file path in 03_train_model.py

--- Assignment 2/scripts/03_train_model.py
+++ Assignment 2/scripts/03_train_model.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 """
 First idea for the model is to use EEGNet. This is a model that is specifically designed for EEG data.
@@ -5,8 +7,10 @@ It should be a good fit for the data that we want to predict and process.
 https://arxiv.org/abs/1611.08024
 """
 
-data = np.load("data/processed/intra_train.npz")
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+data_path = PROJECT_ROOT / "data" / "processed" / "intra_train.npz"
 
+data = np.load(data_path)
 X = data["X"]
 y = data["y"]
 
